9d8e6886a86c5b85dab9009b5f04c3b9d22e5449
Tiberius Lockett <tlockett680@gmail.com>
2022-12-02 09:16:39 -0800

    Add world

---
+++ Experience/World/World.js
@@ -0,0 +1,28 @@
+import Experience from "../Experience.js";
+import * as THREE from "three";
+import Room from "./Room.js";
+
+
+export default class World{
+  constructor(){
+    this.experience = new Experience();
+    this.sizes = this.experience.sizes;
+    this.scene = this.experience.scene;
+    this.canvas = this.experience.canvas;
+    this.camera = this.experience.camera;
+
+    this.room = new Room();
+  }
+  setRenderer(){
+    
+  }
+
+  resize() {
+    
+  }
+  
+  //To Update scene
+  update(){
+    
+  }
+}
