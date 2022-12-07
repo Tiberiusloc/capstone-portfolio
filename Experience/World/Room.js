3cac56a77547f970418ca89bbb98c0adba1ea1a9
Tiberius Lockett <tlockett680@gmail.com>
2022-12-07 11:34:16 -0800

    add shadows, screen display working

--- Experience/World/Room.js
+++ Experience/World/Room.js
@@ -15,7 +15,26 @@ export default class Room{
   }
 
   setModel() {
+this.actualRoom.children.forEach(child => {
+  child.castShadow = true;
+  child.receiveShadow = true;
+
+  if (child instanceof THREE.Group) {
+    child.children.forEach((groupchild) => {
+      groupchild.castShadow = true;
+      groupchild.receiveShadow = true;
+    });
+  }
+
+  if(child.name === "MainMonitorScreen"){
+    child.material = new THREE.MeshBasicMaterial({
+      map: this.resources.items.screen,
+    });
+  }
+});
+
     this.scene.add(this.actualRoom);
+    this.actualRoom.scale.set(0.11, 0.11, 0.11)
   }
 
   setRenderer(){
