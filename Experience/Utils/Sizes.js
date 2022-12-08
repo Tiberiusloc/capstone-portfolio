1a77da86b3af539aa0aeae1bbf564bd4246ff20d
Tiberius Lockett <tlockett680@gmail.com>
2022-12-07 19:57:11 -0800

    update ortho camera size

--- Experience/Utils/Sizes.js
+++ Experience/Utils/Sizes.js
@@ -7,7 +7,7 @@ export default class Sizes extends EventEmitter{
     this.height = window.innerHeight;
     this.aspect = this.width/this.height;
     this.pixelRatio = Math.min(window.devicePixelRatio, 2);
-    this.frustrum = 1.9;
+    this.frustrum = 2.2;
 
     window.addEventListener("resize",()=> {
       this.width = window.innerWidth;
