69a5aa0e3e43922e3493228c1993c7f0d2922339
Tiberius Lockett <tlockett680@gmail.com>
2022-12-07 17:58:28 -0800

    add updates for world render

--- Experience/Experience.js
+++ Experience/Experience.js
@@ -4,6 +4,9 @@ import Time from "./Utils/Time.js";
 import Camera from "./Camera.js";
 import Renderer from "./Renderer.js";
 import World from "./World/World.js";
+import Resources from "./Utils/Resources.js";
+import assets from "./Utils/Assets.js";
+
 
 export default class Experience{
   static instance
@@ -18,6 +21,7 @@ export default class Experience{
     this.sizes = new Sizes();
     this.camera = new Camera();
     this.renderer = new Renderer();
+    this.resources = new Resources(assets);
     this.world = new World();
     
     this.sizes.on("resize", ()=>{
@@ -32,11 +36,13 @@ export default class Experience{
   resize(){
     this.camera.resize();
     this.renderer.resize();
+    this.world.resize();
   }
 
   update(){
     this.camera.update();
+    this.world.update();
     this.renderer.update();
   }
-
+  
 }
