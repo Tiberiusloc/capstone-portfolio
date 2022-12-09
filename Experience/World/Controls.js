d1d85f1c0f6322fe87fcc2de191953a48be4156f
Tiberius Lockett <tlockett680@gmail.com>
2022-12-09 09:10:40 -0800

    add sizes, add scrolltrigger to reposition room on scroll

--- Experience/World/Controls.js
+++ Experience/World/Controls.js
@@ -1,17 +1,37 @@
 import Experience from "../Experience.js";
 import * as THREE from "three";
 import GSAP from "gsap";
+import  { ScrollTrigger }  from "gsap/ScrollTrigger";
 
 export default class Controls {
   constructor() {
     this.experience = new Experience();
     this.scene = this.experience.scene;
+    this.sizes = this.experience.sizes;
     this.resources = this.experience.resources;
     this.time = this.experience.time;
     this.camera = this.experience.camera;
+    this.room = this.experience.world.room.actualRoom;
 
-    this.progress = 0;
-    this.dummyCurve = new THREE.Vector3(0, 0, 0);
+    GSAP.registerPlugin(ScrollTrigger);
+
+    this.setPath();
+  }
+
+  setPath() {
+    console.log(this.room);
+    // To move room to right
+    this.timeline = new GSAP.timeline();
+    this.timeline.to(this.room.position, {
+      x: 0.5,
+      scrollTrigger:{
+        trigger:".first-move",
+        markers: true,
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 0.6,
+      }
+    });
 
   }
 
