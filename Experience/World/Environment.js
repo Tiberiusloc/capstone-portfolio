730264540c9d1eeff0aa00304b43968605cf04c0
Tiberius Lockett <tlockett680@gmail.com>
2022-12-09 12:13:24 -0800

    comment out GUI helper

--- Experience/World/Environment.js
+++ Experience/World/Environment.js
@@ -9,7 +9,7 @@ export default class Environment{
     this.scene = this.experience.scene;
     this.resources = this.experience.resources;
 
-    this.gui = new GUI();
+    //this.gui = new GUI();
     this.obj = {
       colorObj: {r:0, g:0, b:0},
       intensity: 3,
@@ -20,17 +20,17 @@ export default class Environment{
 
   }
 
-  setGUI(){
-    this.gui.addColor(this.obj, "colorObj").onChange(() => {
-      this.sunLight.color.copy(this.obj.colorObj)
-      this.ambientLight.color.copy(this.obj.colorObj)
-      console.log(this.obj.colorObj);
-    });
-    this.gui.add(this.obj, "intensity", 0, 10).onChange(() => {
-      this.sunLight.intensity = this.obj.intensity
-      this.sunLight.intensity = this.obj.intensity
-    });
-  }
+  // setGUI(){
+  //   this.gui.addColor(this.obj, "colorObj").onChange(() => {
+  //     this.sunLight.color.copy(this.obj.colorObj)
+  //     this.ambientLight.color.copy(this.obj.colorObj)
+  //     console.log(this.obj.colorObj);
+  //   });
+  //   this.gui.add(this.obj, "intensity", 0, 10).onChange(() => {
+  //     this.sunLight.intensity = this.obj.intensity
+  //     this.sunLight.intensity = this.obj.intensity
+  //   });
+  // }
 
   setSunlight() {
     this.sunLight = new THREE.DirectionalLight("#ffffff", 3);
