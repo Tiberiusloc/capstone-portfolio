cffd11d57018be14101fac6a4434b97c349a7d5f
Tiberius Lockett <tlockett680@gmail.com>
2022-12-10 12:31:25 -0800

    add navbar WIP drop down menu

---
+++ Experience/Navbar.js
@@ -0,0 +1,39 @@
+import EventEmitter from "events";
+
+export default class Navbar extends EventEmitter{
+  constructor() {
+    super();
+
+    this.navbar = document.querySelectorAll(".navbaritem")
+    
+    this.setEventListeners();
+  }
+
+
+  setEventListeners() {
+    console.log("hit")
+    document.querySelectorAll(".navbarlink").forEach(a => {
+      a.addEventListener("click", function(e){
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href"))
+        .scrollIntoView({
+          behavior: "smooth",
+        });
+      });
+    });
+    
+//     // Close the dropdown if the user clicks outside of it
+//     window.onclick = function(event) {
+//       if (!event.target.matches('.dropbtn')) {
+//         var dropdowns = document.getElementsByClassName("dropdown-content");
+//         var i;
+//         for (i = 0; i < dropdowns.length; i++) {
+//           var openDropdown = dropdowns[i];
+//           if (openDropdown.classList.contains('show')) {
+//             openDropdown.classList.remove('show');
+//           }
+//         }
+//       }
+//     }
+  };
+}
