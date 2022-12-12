800029f76e268e8106406fd26f5a56dd6d834b11
Tiberius Lockett <tlockett680@gmail.com>
2022-12-12 07:38:43 -0800

    update event listener to add mobile supp WIP to reset animation at start

--- Experience/World/Controls.js
+++ Experience/World/Controls.js
@@ -15,28 +15,116 @@ export default class Controls {
 
     GSAP.registerPlugin(ScrollTrigger);
 
-    this.setPath();
+    this.setScrollTrigger();
   }
+  
 
-  setPath() {
-    console.log(this.room);
-    // To move room to right
-    this.timeline = new GSAP.timeline();
-    this.timeline.to(this.room.position, {
-      x: () => {
-        return this.sizes.width * 0.0004;
-      },
-      scrollTrigger:{
-        trigger:".first-move",
-        markers: true,
-        start: "top top",
-        end: "bottom bottom",
-        scrub: 0.7,
-        invalidateOnRefresh: true,
-      }
-    });
+  setScrollTrigger() {
+    this.scrollers = GSAP.matchMedia();
 
-  }
+    this.scrollers.add("(min-width: 969px)", () => {
+      this.firstMoveTimeline = new GSAP.timeline({
+        scrollTrigger: ".first-move",
+          start: "top top",
+          bottom: "bottom bottom",
+          scrub: 1,
+          invalidateOnRefresh: true,
+      });
+      this.firstMoveTimeline.to(this.room.position, {
+        x: () => {
+          return this.sizes.width * 0.00055;
+        },
+      });
+            // Second Section
+          this.secondMoveTimeline = new GSAP.timeline({
+            scrollTrigger:".second-move",
+              start: "top top",
+              bottom: "bottom bottom",
+              scrub: 1,
+              invalidateOnRefresh: true,
+          });
+          this.secondMoveTimeline.to(this.room.position, {
+            x: () => {
+              return 0.7;
+            },
+            z: () => {
+              return this.sizes.height * 0.0012;
+            },
+          },
+            "same"
+          );
+          this.secondMoveTimeline.to(this.room.scale, {
+            x: 0.45,
+            y: 0.45,
+            z: 0.45,
+          },
+            "same"
+          );
+
+          //Third Section
+          this.thirdMoveTimeline = new GSAP.timeline({
+            scrollTrigger: ".third-move",
+              start: "top top",
+              bottom: "bottom bottom",
+              scrub: 1,
+              invalidateOnRefresh: true,
+          }).to(this.camera.orthographicCamera.position ,{
+            y: 0,
+            x: -1.5,
+          });
+      });
+      //Mobile Timeline
+      this.scrollers.add("(max-width: 968px)", () => {
+        this.firstMoveTimeline = new GSAP.timeline({
+          scrollTrigger: ".first-move",
+            start: "top top",
+            bottom: "bottom bottom",
+            scrub: 0.6,
+            invalidateOnRefresh: true,
+        }).to(this.room.scale , {
+          x: 0.14,
+          y: 0.14,
+          z: 0.14,
+        });
+              // Second Section
+            this.secondMoveTimeline = new GSAP.timeline({
+              scrollTrigger:".second-move",
+                start: "top top",
+                bottom: "bottom bottom",
+                scrub: 0.6,
+                invalidateOnRefresh: true,
+            })
+            this.secondMoveTimeline.to(this.room.position, {
+              x: () => {
+                return 1;
+              },
+              z: () => {
+                return this.sizes.height * 0.0012;
+              },
+            },
+              "same"
+            )
+            this.secondMoveTimeline.to(this.room.scale, {
+              x: 0.45,
+              y: 0.45,
+              z: 0.45,
+            },
+              "same"
+            )
+  
+            //Third Section
+            this.thirdMoveTimeline = new GSAP.timeline({
+              scrollTrigger: ".third-move",
+                start: "top top",
+                bottom: "bottom bottom",
+                scrub: 0.6,
+                invalidateOnRefresh: true,
+            }).to(this.camera.orthographicCamera.position ,{
+              y: 0,
+              x: -1,
+            })
+      })
+    }
 
   resize() {
     
