e6f13a6ca1d3992bcc0fa72f565012c9691e8437
Tiberius Lockett <tlockett680@gmail.com>
2022-12-13 20:18:48 -0800

    Add timelines for mobile and desktop media

--- Experience/World/Controls.js
+++ Experience/World/Controls.js
@@ -20,118 +20,211 @@ export default class Controls {
   
 
   setScrollTrigger() {
-    this.scrollers = GSAP.matchMedia();
 
-    this.scrollers.add("(min-width: 969px)", () => {
+    
+    ScrollTrigger.matchMedia({"(min-width: 969px)": () => {
+
       this.firstMoveTimeline = new GSAP.timeline({
-        scrollTrigger: ".first-move",
-          start: "top top",
-          bottom: "bottom bottom",
-          scrub: 1,
-          invalidateOnRefresh: true,
+        scrollTrigger: { 
+        trigger:".first-move",
+        start: "top top",
+        bottom: "bottom bottom",
+        scrub: 1,
+        invalidateOnRefresh: true,
+        },
       });
       this.firstMoveTimeline.to(this.room.position, {
         x: () => {
           return this.sizes.width * 0.00055;
         },
       });
-            // Second Section
-          this.secondMoveTimeline = new GSAP.timeline({
-            scrollTrigger:".second-move",
-              start: "top top",
-              bottom: "bottom bottom",
-              scrub: 1,
-              invalidateOnRefresh: true,
+      // Second Section
+      this.secondMoveTimeline = new GSAP.timeline({
+        scrollTrigger: {
+        trigger: ".second-move",
+        start: "top top",
+        bottom: "bottom bottom",
+        scrub: 1,
+        invalidateOnRefresh: true,
+        }
+      });
+      this.secondMoveTimeline.to(this.room.position, {
+        x: () => {
+          return 1;
+        },
+        z: () => {
+          return this.sizes.height * 0.0012;
+        },
+      },
+      "same"
+      );
+      this.secondMoveTimeline.to(this.room.scale, {
+        x: 0.45,
+        y: 0.45,
+        z: 0.45,
+      },
+      "same"
+      );
+
+      
+      //Third Section
+      this.thirdMoveTimeline = new GSAP.timeline({
+        scrollTrigger: {
+          trigger: ".third-move",
+          start: "top top",
+          bottom: "bottom bottom",
+          scrub: 1,
+          invalidateOnRefresh: true,
+        },
+          }).to(this.camera.orthographicCamera.position ,{
+            y: 0,
+            x: -1.5,
           });
+        }
+      });
+      
+        //Mobile Timeline
+        ScrollTrigger.matchMedia({"(max-width: 968px)": () => {
+          console.log("mobile");
+          this.firstMoveTimeline = new GSAP.timeline({
+            scrollTrigger: {
+            trigger: ".first-move",
+            start: "top top",
+            bottom: "bottom bottom",
+            scrub: 0.6,
+            invalidateOnRefresh: true,
+            },
+          }).to(this.room.scale , {
+            x: 0.14,
+            y: 0.14,
+            z: 0.14,
+          });
+          // Second Section
+          this.secondMoveTimeline = new GSAP.timeline({
+            scrollTrigger: {
+            trigger: ".second-move",
+            start: "top top",
+            bottom: "bottom bottom",
+            scrub: 0.6,
+            invalidateOnRefresh: true,
+            },
+          })
           this.secondMoveTimeline.to(this.room.position, {
             x: () => {
-              return 0.7;
+              return 1.5;
             },
             z: () => {
               return this.sizes.height * 0.0012;
             },
           },
-            "same"
-          );
+          "same"
+          )
           this.secondMoveTimeline.to(this.room.scale, {
             x: 0.45,
             y: 0.45,
             z: 0.45,
           },
-            "same"
-          );
-
+          "same"
+          )
+          
           //Third Section
           this.thirdMoveTimeline = new GSAP.timeline({
-            scrollTrigger: ".third-move",
-              start: "top top",
-              bottom: "bottom bottom",
-              scrub: 1,
-              invalidateOnRefresh: true,
-          }).to(this.camera.orthographicCamera.position ,{
-            y: 0,
-            x: -1.5,
-          });
-      });
-      //Mobile Timeline
-      this.scrollers.add("(max-width: 968px)", () => {
-        this.firstMoveTimeline = new GSAP.timeline({
-          scrollTrigger: ".first-move",
+            scrollTrigger: {
+            trigger: ".third-move",
             start: "top top",
             bottom: "bottom bottom",
-            scrub: 0.6,
+            scrub: 1,
             invalidateOnRefresh: true,
-        }).to(this.room.scale , {
-          x: 0.14,
-          y: 0.14,
-          z: 0.14,
-        });
-              // Second Section
-            this.secondMoveTimeline = new GSAP.timeline({
-              scrollTrigger:".second-move",
-                start: "top top",
-                bottom: "bottom bottom",
-                scrub: 0.6,
-                invalidateOnRefresh: true,
-            })
-            this.secondMoveTimeline.to(this.room.position, {
-              x: () => {
-                return 1;
-              },
-              z: () => {
-                return this.sizes.height * 0.0012;
-              },
             },
-              "same"
-            )
-            this.secondMoveTimeline.to(this.room.scale, {
-              x: 0.45,
-              y: 0.45,
-              z: 0.45,
+          }).to(this.camera.orthographicCamera.position ,{
+            y: -1,
+            x: 0,
+          })
+          
+        },
+      });
+      
+      
+
+      ScrollTrigger.matchMedia({"all": () => {
+          this.secondPartTimeline = new GSAP.timeline({
+            scrollTrigger: {
+              trigger: ".third-move",
+              start: "center center",
             },
-              "same"
-            )
-  
-            //Third Section
-            this.thirdMoveTimeline = new GSAP.timeline({
-              scrollTrigger: ".third-move",
-                start: "top top",
-                bottom: "bottom bottom",
-                scrub: 0.6,
-                invalidateOnRefresh: true,
-            }).to(this.camera.orthographicCamera.position ,{
-              y: 0,
-              x: -1,
-            })
-      })
-    }
+          });
+            
+          this.room.children.forEach((child) => {
+            if(child.name === "MiniFloor"){
+              this.first = GSAP.to(child.position, {
+                x: 2.4,
+                z: 0,
+                ease: "back.out(2)",
+                duration: 1,
+              });
+            }
+            if (child.name === 'Mailbox') {
+              this.second = GSAP.to(child.scale, {
+                x: 1,
+                y: 1,
+                z: 1,
+                ease: "back.out(2)",
+                duration: 0.3,
+              });
+            }
+            if (child.name === 'Lamp') {
+              this.third = GSAP.to(child.scale, {
+                x: 1,
+                y: 1,
+                z: 1,
+                ease: "back.out(2)",
+                duration: 0.3,
+              });
+            }
+            if (child.name === 'FloorFirst') {
+              this.fourth = GSAP.to(child.scale, {
+                x: 1,
+                y: 1,
+                z: 1,
+                ease: "back.out(2)",
+                duration: 0.3,
+              });
+            }
+            if (child.name === 'FloorSecond') {
+              this.fifth = GSAP.to(child.scale, {
+                x: 1,
+                y: 1,
+                z: 1,
+                ease: "back.out(2)",
+                duration: 0.3,
+              });
+            }
+            if (child.name === 'FloorThird') {
+              this.sixth = GSAP.to(child.scale, {
+                x: 1,
+                y: 1,
+                z: 1,
+                ease: "back.out(2)",
+                duration: 0.3,
+              });
+            }
+          });
+          this.secondPartTimeline.add(this.first);
+          this.secondPartTimeline.add(this.second);
+          this.secondPartTimeline.add(this.third);
+          this.secondPartTimeline.add(this.fourth);
+          this.secondPartTimeline.add(this.fifth);
+          this.secondPartTimeline.add(this.sixth);
 
-  resize() {
-    
+      },
+    });
   }
-  
-  //To Update scene
-  update(){
-
-  }
+      
+      resize() {
+        
+      }
+      
+      //To Update scene
+      update(){
+      }
 }
