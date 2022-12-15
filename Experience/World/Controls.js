f3f1e3987143f3d8625aa6508993010add3eaba1
Tiberius Lockett <tlockett680@gmail.com>
2022-12-14 23:39:29 -0800

    Add proxy for smooth scroll

--- Experience/World/Controls.js
+++ Experience/World/Controls.js
@@ -2,6 +2,7 @@ import Experience from "../Experience.js";
 import * as THREE from "three";
 import GSAP from "gsap";
 import  { ScrollTrigger }  from "gsap/ScrollTrigger";
+import ASScroll from "@ashthornton/asscroll";
 
 export default class Controls {
   constructor() {
@@ -15,7 +16,49 @@ export default class Controls {
 
     GSAP.registerPlugin(ScrollTrigger);
 
+    this.setSmoothScroll();
     this.setScrollTrigger();
+  }
+
+  setupASScroll() {
+    // https://github.com/ashthornton/asscroll
+    const asscroll = new ASScroll({
+      disableRaf: true });
+  
+  
+    GSAP.ticker.add(asscroll.update);
+  
+    ScrollTrigger.defaults({
+      scroller: asscroll.containerElement });
+  
+  
+    ScrollTrigger.scrollerProxy(asscroll.containerElement, {
+      scrollTop(value) {
+        if (arguments.length) {
+          asscroll.currentPos = value;
+          return;
+        }
+        return asscroll.currentPos;
+      },
+      getBoundingClientRect() {
+        return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+      },
+      fixedMarkers: true });
+  
+  
+    asscroll.on("update", ScrollTrigger.update);
+    ScrollTrigger.addEventListener("refresh", asscroll.resize);
+  
+    requestAnimationFrame(() => {
+      asscroll.enable({
+        newScrollElements: document.querySelectorAll(".gsap-marker-start, .gsap-marker-end, [asscroll]") });
+  
+    });
+    return asscroll;
+  }
+
+  setSmoothScroll() {
+    this.asscroll = this.setupASScroll();
   }
   
 
@@ -147,6 +190,66 @@ export default class Controls {
       
 
       ScrollTrigger.matchMedia({"all": () => {
+
+        this.sections = document.querySelectorAll(".section");
+        this.sections.forEach((section) => {
+          this.progressWrapper = section.querySelector(".progress-wrapper");
+          this.progressBar = section.querySelector(".progress-bar");
+
+          if (section.classList.contains("right")){
+            
+            GSAP.to(section, {
+              borderTopLeftRadius: 10,
+              scrollTrigger: {
+                trigger: section,
+                start: "top bottom",
+                end: "top top",
+                scrub: 1,
+              }
+            });
+            GSAP.to(section, {
+              borderBottomLeftRadius: 700,
+              scrollTrigger: {
+                trigger: section,
+                start: "bottom bottom",
+                end: "bottom top",
+                scrub: 1,
+              }
+            });
+            } else {
+              GSAP.to(section, {
+                borderTopRightRadius: 10,
+                scrollTrigger: {
+                  trigger: section,
+                  start: "top bottom",
+                  end: "top top",
+                  scrub: 1,
+                }
+              });
+              GSAP.to(section, {
+                borderBottomRightRadius: 700,
+                scrollTrigger: {
+                  trigger: section,
+                  start: "bottom bottom",
+                  end: "bottom top",
+                  scrub: 1,
+                }
+              });
+            }
+            GSAP.from(this.progressBar, {
+              scaleY: 0,
+              scrollTrigger: {
+                trigger: section,
+                start: "top top",
+                end: "bottom bottom",
+                scrub: 0.2,
+                pin: this.progressWrapper,
+                pinSpacing: false,
+              }
+            });
+          });
+
+        //Animations
           this.secondPartTimeline = new GSAP.timeline({
             scrollTrigger: {
               trigger: ".third-move",
