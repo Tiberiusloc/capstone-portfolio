c4153bed019500d1a4395721cee035b396a1ae01
Tiberius Lockett <tlockett680@gmail.com>
2022-12-15 10:40:03 -0800

    add circle material add circle positioning and scale

--- Experience/World/Floor.js
+++ Experience/World/Floor.js
@@ -7,6 +7,8 @@ export default class Floor {
     this.scene = this.experience.scene;
 
     this.setFloor();
+    this.setCircles();
+
 
   }
 
@@ -23,6 +25,41 @@ export default class Floor {
     this.plane.receiveShadow = true;
   }
 
+  setCircles() {
+    const geometry = new THREE.CircleGeometry(5, 32);
+    const material = new THREE.MeshStandardMaterial({ color: 0xb8a97d});
+    const material2 = new THREE.MeshStandardMaterial({ color: 0x8395cd });
+    const material3 = new THREE.MeshStandardMaterial({ color: 0x7ad0ac });
+
+    this.circleFirst = new THREE.Mesh(geometry, material);
+    this.circleSecond = new THREE.Mesh(geometry, material2);
+    this.circleThird = new THREE.Mesh(geometry, material3);
+
+    this.circleFirst.position.y = 0;
+
+    this.circleSecond.position.y = 0.1;
+    this.circleSecond.position.x = 0;
+
+    this.circleThird.position.y = 0.2;
+
+    this.circleFirst.scale.set(0, 0, 0);
+    this.circleSecond.scale.set(0, 0, 0);
+    this.circleThird.scale.set(0, 0, 0);
+
+    this.circleFirst.rotation.x =
+        this.circleSecond.rotation.x =
+        this.circleThird.rotation.x =
+            -Math.PI / 2;
+
+    this.circleFirst.receiveShadow =
+        this.circleSecond.receiveShadow =
+        this.circleThird.receiveShadow =
+            true;
+
+    this.scene.add(this.circleFirst);
+    this.scene.add(this.circleSecond);
+    this.scene.add(this.circleThird);
+}
 
   resize() {
     
