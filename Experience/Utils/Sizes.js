0b131644cf1576995735b8ff3701ef50fc5890f2
Tiberius Lockett <tlockett680@gmail.com>
2022-12-21 19:17:58 -0800

    clean up logs

--- Experience/Utils/Sizes.js
+++ Experience/Utils/Sizes.js
@@ -25,11 +25,9 @@ export default class Sizes extends EventEmitter{
 
       if(this.width < 968 && this.device !== "mobile") {
         this.device = "mobile";
-        console.log("mobile");
         this.emit("switchdevice", this.device);
       } else if (this.width >= 968 && this.device !== "desktop") {
         this.device = "desktop";
-        console.log("desktop");
         this.emit("switchdevice", this.device);
       }
     })
